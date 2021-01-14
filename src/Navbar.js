b0fc358c0329236dc7fb329c049998ec7b022efa
Marcelin <marsbob@live.fr>
2021-01-14 11:27:06 +0100

    Fin exercice 3

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import './index.css'
+function Navbar() {
+    return(
+        <nav>
+            <ul>
+                <li>
+                    home
+                </li>
+                <li>
+                    contact
+                </li>
+                <li>
+                    portfolio
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
